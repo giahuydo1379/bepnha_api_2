3203af9471bbb0e00fc9559ddf733f06657217e2
giahuydo1379 <giahuyplaygame@gamil.com>
2018-09-27 15:31:00 +0700

    new

--- app/Http/Controllers/bepnha/VideoController.php
+++ app/Http/Controllers/bepnha/VideoController.php
@@ -13,86 +13,98 @@ use DB;
 class VideoController extends Controller
 {
     public static $model;
+
     public function __construct()
     {
         $this::$model = new Videos();
         Carbon::setLocale('vi');
     }
-    private function getQuery($uid) {
+
+    private function getQuery($uid)
+    {
         $days = DB::raw("videos.date_created as days");
-        $image = DB::raw('concat("'.env('MEDIA_URL_IMAGE').'/",videos.image_location) as image');
-        $video = DB::raw('concat("'.env('MEDIA_URL_VIDEO').'/",videos.video_location) as video');
+        $image = DB::raw('concat("' . env('MEDIA_URL_IMAGE') . '/",videos.image_location) as image');
+        $video = DB::raw('concat("' . env('MEDIA_URL_VIDEO') . '/",videos.video_location) as video');
         $liked = DB::raw('notebook.video_id IS NOT NULL as liked');
         $query = DB::table('videos')
             ->leftJoin('notebook', 'videos.id', '=', 'notebook.video_id', 'and', 'notebook.user_id', '=', $uid)
             ->select('videos.id', 'videos.name', $image, $video,
-                'videos.description', 'videos.chef', 'videos.ingredients', 'videos.ingredients_2','videos.steps',
+                'videos.description', 'videos.chef', 'videos.ingredients', 'videos.ingredients_2', 'videos.steps',
                 'videos.duration', 'videos.time_to_done', 'videos.level', 'videos.note',
                 $days, 'videos.video_type_id as kind', 'videos.view_count', $liked)
             ->where('videos.disable', '=', '0')->orderby('videos.date_created', 'desc')->distinct();
         return $query;
     }
-    public function getVideo(Request $request, $id) {
+
+    public function getVideo(Request $request, $id)
+    {
         $user_id = $request->input('uid');
         $result = array();
         try {
             $result['data'] = $this->getQuery($user_id)->find($id);
             $result['status'] = 200;
-        }
-        catch(QueryException $e) {
+        } catch (QueryException $e) {
             $result['status'] = $e->getCode();
             $result['errMsg'] = $e->getMessage();
         }
         return $result;
     }
-    public function getVideos(Request $request) {
+
+    public function getVideos(Request $request)
+    {
         $limit = $request->input('limit', 10);
         $page = $request->input('page', 1);
         $uid = $request->input('uid');
         $result = array();
         try {
             $query = $this->getQuery($uid);
-            $query->skip($limit * ($page-1))->take($limit);
-            if($page == 1)
+            $query->skip($limit * ($page - 1))->take($limit);
+            if ($page == 1)
                 $query->take($limit);
             else
-                $query->skip($limit * ($page-1))->take($limit);
+                $query->skip($limit * ($page - 1))->take($limit);
 
             $data = $query->get();
-            foreach ($data as $item){
+            foreach ($data as $item) {
                 $item->days = Carbon::createFromTimeStamp(strtotime($item->days))->diffForHumans();
-                $notebook = DB::table('notebook')->where('video_id',$item->id)->get();
-                if(isset($notebook) && count($notebook)>0){
-                    foreach ($notebook as $value){
-                        if( $value->user_id == $uid){
+                $notebook = DB::table('notebook')->where('video_id', $item->id)->get();
+                if (isset($notebook) && count($notebook) > 0) {
+                    foreach ($notebook as $value) {
+                        if ($value->user_id == $uid) {
                             $item->liked = 1;
                             break;
-                        }else{
+                        } else {
                             $item->liked = 0;
                         }
                     }
-                }else{
+                } else {
                     $item->liked = 0;
                 }
             }
             $result['data'] = $data;
             $result['status'] = 200;
-        } catch(QueryException $e) {
+        } catch (QueryException $e) {
             $result['status'] = $e->getCode();
             $result['errMsg'] = $e->getMessage();
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             $result['status'] = $e->getCode();
             $result['errMsg'] = $e->getMessage();
         }
         return $result;
     }
-    public function incViewCount($id) {
+
+    public function incViewCount($id)
+    {
         $result = array();
         try {
             $this::$model->where('id', $id)->increment('view_count');
+
+
+            $logView = DB::table('log_view')->insertGetId(['vid_doc_id' => $id, 'type' => '1', 'date' => Carbon::today()]);
+
+
             $result['status'] = 200;
-        }
-        catch(QueryException $e) {
+        } catch (QueryException $e) {
             $result['status'] = $e->getCode();
             $result['errMsg'] = $e->getMessage();
         }
@@ -100,41 +112,41 @@ class VideoController extends Controller
     }
 
 
-    public function Search(Request $request) {
+    public function Search(Request $request)
+    {
         $key = $request->input('key');
         $uid = $request->input('uid');
         $limit = $request->input('limit', 10);
         $page = $request->input('page', 1);
-        $result = array('status'=>'');
+        $result = array('status' => '');
         try {
-            if($key !== null) {
+            if ($key !== null) {
                 $query = $this->getQuery($uid)
-                    ->where('videos.name','like', "%$key%");
-                if($page == 1)
+                    ->where('videos.name', 'like', "%$key%");
+                if ($page == 1)
                     $query->take($limit);
                 else
-                    $query->skip($limit * ($page-1))->take($limit);
+                    $query->skip($limit * ($page - 1))->take($limit);
                 $data = $query->get();
-                foreach ($data as $item){
+                foreach ($data as $item) {
                     $item->days = Carbon::createFromTimeStamp(strtotime($item->days))->diffForHumans();
-                    $notebook = DB::table('notebook')->where('video_id',$item->id)->get();
-                    if(isset($notebook) && count($notebook)>0){
-                        foreach ($notebook as $value){
-                            if( $value->user_id == $uid){
+                    $notebook = DB::table('notebook')->where('video_id', $item->id)->get();
+                    if (isset($notebook) && count($notebook) > 0) {
+                        foreach ($notebook as $value) {
+                            if ($value->user_id == $uid) {
                                 $item->liked = 1;
                                 break;
-                            }else{
+                            } else {
                                 $item->liked = 0;
                             }
                         }
-                    }else{
+                    } else {
                         $item->liked = 0;
                     }
                 }
                 $result['data'] = $data;
                 $result['status'] = 200;
-            }
-            else {
+            } else {
                 $result['status'] = 404;
             }
         } catch (QueryException $e) {
@@ -143,31 +155,5 @@ class VideoController extends Controller
         }
         return $result;
     }
-	/*public function Search2(Request $request){
-		$key = $request->input('key');
-		$limit = $request->input('limit', 10);
-		$page = $request->input('page', 1);
-		$result = array('status'=>'');
-		try{
-			if ($key !== null){
-				$query = DB::table('videos')->where('name','like',"%$key%")
-					->orderby('date_created','desc');
-				if($page == 1)
-					$query->take($limit);
-				else
-					$query->skip($limit * ($page-1))->take($limit);
-				$data = $query->get();
-				$result['data'] = $data;
-				$result['status'] = 200;
-			}
-			else{
-				$result['status'] = 404;
-			}
-		}
-		catch (\Exception $e){
-			$result['status'] = $e->getCode();
-			$result['errMsg'] = $e->getMessage();
-		}
-		return $result;
-	}*/
+
 }
---
+++ app/Http/Models/LogVidDoc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogVidDoc extends Model
+{
+	public $table = 'log_vid_doc';
+	public $timestamps = true;
+}
